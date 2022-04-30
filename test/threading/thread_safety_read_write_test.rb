73862567b425502094a0a5451942f2625ad0af95
Jan Biedermann <jan@kursator.de>
2022-04-30 22:09:21 +0200

    use common temp dir

--- test/threading/thread_safety_read_write_test.rb
+++ test/threading/thread_safety_read_write_test.rb
@@ -5,7 +5,7 @@ require 'thread'
 class IndexThreadSafetyReadWriteTest < Test::Unit::TestCase
   include Isomorfeus::Ferret::Index
 
-  INDEX_DIR = File.expand_path(File.join(File.dirname(__FILE__), "index"))
+  INDEX_DIR = File.expand_path(File.join(File.dirname(__FILE__), ".." , "temp", "threading"))
   ITERATIONS = 1000
   NUM_THREADS = 3
   ANALYZER = Isomorfeus::Ferret::Analysis::Analyzer.new
