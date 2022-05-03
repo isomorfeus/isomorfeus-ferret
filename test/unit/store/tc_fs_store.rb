43e0b93caf628ab58a917cadca049f34d98bb027
Jan Biedermann <jan@kursator.de>
2022-05-04 00:37:00 +0200

    style

--- test/unit/store/tc_fs_store.rb
+++ test/unit/store/tc_fs_store.rb
@@ -9,8 +9,7 @@ class FSStoreTest < Test::Unit::TestCase
   include StoreTest
   include StoreLockTest
   def setup
-    @dpath = File.expand_path(File.join(File.dirname(__FILE__),
-                       '../../temp/fsdir'))
+    @dpath = File.expand_path(File.join(File.dirname(__FILE__), '../../temp/fsdir'))
     @dir = FSDirectory.new(@dpath, true)
   end
 
