7d0d277693a20beaee7e993ca943576d6f8fe298
Jan Biedermann <jan@kursator.de>
2022-05-10 05:15:44 +0200

    use different index dirs for threading tests

--- test/threading/thread_safety_read_write_test.rb
+++ test/threading/thread_safety_read_write_test.rb
@@ -5,7 +5,7 @@ require 'thread'
 class IndexThreadSafetyReadWriteTest < Test::Unit::TestCase
   include Isomorfeus::Ferret::Index
 
-  INDEX_DIR = File.expand_path(File.join(File.dirname(__FILE__), ".." , "temp", "threading"))
+  INDEX_DIR = File.expand_path(File.join(File.dirname(__FILE__), ".." , "temp", "threading_rw"))
   ITERATIONS = 100
   NUM_THREADS = 3
   ANALYZER = Isomorfeus::Ferret::Analysis::Analyzer.new
